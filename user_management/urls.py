ecf0a4b76eeec6b94c3762b245778f9d238b624b
mie-jejaw <mierafjejaw@gmail.com>
2024-04-01 14:06:02 +0300

    Refactor: Eliminate trailing slashes from endpoints

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 import user_management.views as views
 urlpatterns = [
-    path('users/register/', views.UserRegistrationView.as_view(), name='user-registration'),
-    path('users/delete/<uuid:id>/', views.UserDeleteView.as_view(), name='user-delete-by-id'),
-    path('users/get-all/', views.UserListView.as_view(), name='user-list'),
-    path('users/is-email-exists/<str:email>/', views.CheckEmailExistsView.as_view(), name='check-email-exists'),
-    path('users/get-by-id/<uuid:id>/', views.UserRetrieveByIdView.as_view(), name='user-retrieve-by-id'),
-    path('users/get-by-email/<str:email>/', views.UserRetrieveByEmailView.as_view(), name='user-retrieve-by-email'),
-    path('users/update/', views.UserUpdateView.as_view(), name='user-update-by-id'),
-    path('users/verify-email/<str:token>/', views.VerifyEmailView.as_view(), name='verify-email'),
+    path('users/register', views.UserRegistrationView.as_view(), name='user-registration'),
+    path('users/delete/<uuid:id>', views.UserDeleteView.as_view(), name='user-delete-by-id'),
+    path('users/get-all', views.UserListView.as_view(), name='user-list'),
+    path('users/is-email-exists/<str:email>', views.CheckEmailExistsView.as_view(), name='check-email-exists'),
+    path('users/get-by-id/<uuid:id>', views.UserRetrieveByIdView.as_view(), name='user-retrieve-by-id'),
+    path('users/get-by-email/<str:email>', views.UserRetrieveByEmailView.as_view(), name='user-retrieve-by-email'),
+    path('users/update', views.UserUpdateView.as_view(), name='user-update-by-id'),
+    path('users/verify-email/<str:token>', views.VerifyEmailView.as_view(), name='verify-email'),
 ]
