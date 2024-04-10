004b2e0782eb222b20cd8186b23a262b247f684a
mie-jejaw <mierafjejaw@gmail.com>
2024-04-10 10:56:21 +0300

    alter the path of converter and remote_handler

--- converter/urls.py
+++ converter/urls.py
@@ -2,8 +2,8 @@ from rest_framework.urls import path
 import converter.views as views
 
 urlpatterns = [
-    path('convert', views.ConverterView.as_view(), name='convert'),
-    path('remote-converter', views.ConvertUsingRemoteAPIView.as_view(), name='converter'),
+    path('converter/convert', views.ConverterView.as_view(), name='convert'),
+    path('converter/remote-converter', views.ConvertUsingRemoteAPIView.as_view(), name='converter'),
 ]
 
 
--- remote_handler/urls.py
+++ remote_handler/urls.py
@@ -2,7 +2,7 @@ from rest_framework.urls import path
 import remote_handler.views as views
 
 urlpatterns = [
-    path('remote-api-token/create', views.RemoteAPITokenManagerCreateView.as_view(), name='remote-api-token-create'),
-    path('remote-api-token/list', views.RemoteAPITokenManagerListView.as_view(), name='remote-api-token-list'),
-    path('remote-api-token/delete/<uuid:id>', views.RemoteAPITokenManagerDeleteView.as_view(), name='remote-api-token-delete'),
+    path('api-token/create', views.RemoteAPITokenManagerCreateView.as_view(), name='remote-api-token-create'),
+    path('api-token/list', views.RemoteAPITokenManagerListView.as_view(), name='remote-api-token-list'),
+    path('api-token/delete/<uuid:id>', views.RemoteAPITokenManagerDeleteView.as_view(), name='remote-api-token-delete'),
 ]
