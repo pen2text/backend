b8bc25b8fdb0ae56a2d05bbc577fe3f7756f34bc
mie-jejaw <mierafjejaw@gmail.com>
2024-04-28 00:43:05 +0300

    improve password validation for registration

--- user_management/serializers.py
+++ user_management/serializers.py
@@ -2,6 +2,7 @@ import re
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
     
@@ -15,18 +16,28 @@ class UserSerializer(serializers.ModelSerializer):
             'role': {'read_only': True},  
         }
  
-
     def validate_gender(self, value):
         if value.lower() not in ['male', 'female']:
             raise serializers.ValidationError("Invalid value for gender field.")
-        return value
+        return value.lower()
             
-    def validate_password(self, value):
-        if value:
-            if not re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$', value):
-                raise serializers.ValidationError("Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be between 8 and 30 characters long.")
-        return value
-
+    def validate_password(self, password):
+        errors = []   
+        
+        if len(password) < 8:
+            errors.append("Password must be at least 8 characters long.")
+        if not re.search(r'[a-z]', password):
+            errors.append("Password must contain at least one lower-case letter.")
+        if not re.search(r'[A-Z]', password):
+            errors.append("Password must contain at least one upper-case letter.")
+        if not re.search(r'[0-9]', password):
+            errors.append("Password must contain at least one digit.")
+        if not re.search(r'[@_!#$%^&*()<>?/\|}{~:=+-.,\[\]]', password):
+            errors.append("Password must contain at least one special character.")
+        if errors:
+            raise serializers.ValidationError(errors)
+            
+        return password
 
     def save(self, **kwargs):
         user = User(
@@ -36,6 +47,7 @@ class UserSerializer(serializers.ModelSerializer):
             gender=self.validated_data.get('gender', ''),
             date_of_birth=self.validated_data.get('date_of_birth', ''),
         )
+        
         # Set and hash the password
         user.set_password(self.validated_data['password'])
         user.save()
