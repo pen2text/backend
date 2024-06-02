3f5774b15f12d38e5224316440f07652c8754045
mie-jejaw <mikeyasalemu47@gmail.com>
2024-06-03 01:16:34 +0300

    refactor and test update and get by id user

--- user_management/tests/views/test_get_by_id.py
+++ user_management/tests/views/test_get_by_id.py
@@ -4,57 +4,23 @@
 
 # @pytest.mark.django_db
 # class TestUserRetrieveByIdView:
-#     def test_admin_retrieves_user_data(self, api_client, mock_token, users):
-#         admin_user = users[0]
-#         target_user = users[1]  # Non-admin user
-#         token = mock_token(admin_user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-
-#         url = reverse('user-retrieve-by-id', kwargs={'id': target_user.id})
-#         response = api_client.get(url)
-
-#         assert response.status_code == status.HTTP_200_OK
-#         assert response.data['data']['id'] == str(target_user.id)
 
 #     def test_user_retrieves_own_data(self, api_client, mock_token, users):
-#         user = users[1]  # Regular user
+#         user = users[1]
 #         token = mock_token(user)
 #         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
 
-#         url = reverse('user-retrieve-by-id', kwargs={'id': user.id})
+#         url = reverse('user-retrieve')
 #         response = api_client.get(url)
 
 #         assert response.status_code == status.HTTP_200_OK
-#         assert response.data['data']['id'] == str(user.id)
-
-#     def test_user_retrieves_other_user_data(self, api_client, mock_token, users):
-#         user = users[1]  # Regular user
-#         token = mock_token(user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-
-#         other_user_id = users[0].id  # Admin user
-#         url = reverse('user-retrieve-by-id', kwargs={'id': other_user_id})
-#         response = api_client.get(url)
-
-#         assert response.status_code == status.HTTP_403_FORBIDDEN
-#         assert response.data['message'] == "Forbidden: You do not have permission to access this resource"
-
-#     def test_admin_retrieves_non_existent_user_data(self, api_client, mock_token, users):
-#         user = users[0]  # Regular user
-#         token = mock_token(user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-
-#         non_existent_user_id = '999e4567-e89b-12d3-a456-426614174099'
-#         url = reverse('user-retrieve-by-id', kwargs={'id': non_existent_user_id})
-#         response = api_client.get(url)
-
-#         assert response.status_code == status.HTTP_404_NOT_FOUND
-#         assert response.data['message'] == "User not found with the provided ID"
+#         assert response.data['status'] == 'OK'
+#         assert response.data['message'] == 'User data retrieved successfully'
 
 #     def test_unauthorized_user_retrieves_users_data(self, api_client, mock_token, users):
 #         api_client.credentials(HTTP_AUTHORIZATION='')
 
-#         url = reverse('user-list')
+#         url = reverse('user-retrieve')
 #         response = api_client.get(url)
 
 #         assert response.status_code == status.HTTP_401_UNAUTHORIZED
--- user_management/tests/views/test_update_user.py
+++ user_management/tests/views/test_update_user.py
@@ -1,107 +1,98 @@
 # import pytest
 # from django.urls import reverse
 # from rest_framework import status
-# from django.contrib.auth.hashers import check_password
 
-
+# @pytest.fixture
+# def url():
+#     return reverse('user-update')
 
 # @pytest.mark.django_db
 # class TestUserUpdateView:
 
-#     def test_update_own_information(self, api_client, mock_token, users):
-#         user = users[0]
-#         token = mock_token(user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+#     @pytest.fixture(autouse=True)
+#     def setup(self, api_client, mock_token, users, url):
+#         self.api_client = api_client
+#         self.mock_token = mock_token
+#         self.users = users
+#         self.url = url
 
-#         url = reverse('user-update')
-#         data = {'id': user.id, 'first_name': 'New Name'}
+#     def test_update_success(self):
+#         user = self.users[0]
+#         token = self.mock_token(user)
+#         self.api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
 
-#         response = api_client.patch(url, data, format='json')
+#         data = {'first_name': 'New Name'}
+
+#         response = self.api_client.patch(self.url, data, format='json')
+
+#         assert response.status_code == status.HTTP_200_OK
+#         assert response.data['status'] == 'OK'
+#         assert response.data['message'] == 'User information updated successfully'
+#         assert response.data['data']['first_name'] == 'New Name'
+        
+#     def test_update_with_invalid_data(self):
+#         user = self.users[0]
+#         token = self.mock_token(user)
+#         self.api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+
+#         data = {'date_of_birth': 'invalid_date'}
+#         response = self.api_client.patch(self.url, data, format='json')
+
+#         assert response.status_code == status.HTTP_400_BAD_REQUEST
+#         response_data = response.json()
+#         assert response_data['status'] == 'FAILED'
+#         assert response_data['message'] == 'Validation failed'
+#         assert 'date_of_birth' in response_data['errors']
+
+#     def test_update_password_success(self):
+#         user = self.users[0]
+#         token = self.mock_token(user)
+#         self.api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+
+#         data = {
+#             'password': 'NewPassword123!',
+#             'old_password': 'JaneDoe123!'
+#         }
+#         response = self.api_client.patch(self.url, data, format='json')
 
 #         assert response.status_code == status.HTTP_200_OK
 #         response_data = response.json()
 #         assert response_data['status'] == 'OK'
 #         assert response_data['message'] == 'User information updated successfully'
-#         assert response_data['data']['first_name'] == 'New Name'
-        
-#     def test_update_other_user_information(self, api_client, mock_token, users):
-#         user = users[1]
-#         token = mock_token(user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-
-#         other_user = users[0]
-#         url = reverse('user-update')
-#         data = {'id': other_user.id, 'first_name': 'New Name'}
-#         response = api_client.patch(url, data, format='json')
-
-#         assert response.status_code == status.HTTP_403_FORBIDDEN
-
-#     def test_update_with_invalid_data(self, api_client, mock_token, users):
-#         user = users[0]
-#         token = mock_token(user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-
-#         url = reverse('user-update')
-#         data = {'id': user.id, 'date_of_birth': 'invalid_email'}
-#         response = api_client.put(url, data, format='json')
-
-#         assert response.status_code == status.HTTP_400_BAD_REQUEST
-#         assert response.data['status'] == 'FAILED'
-#         assert response.data['message'] == 'Validation failed'
-#         assert 'date_of_birth' in response.data['errors']
-
-#     def test_update_password(self, api_client, mock_token, users):
-#         user = users[0]
-#         token = mock_token(user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-
-#         url = reverse('user-update')
-#         data = {
-#             'id': user.id,
-#             'password': 'NewPassword123!',
-#             'old_password': 'JaneDoe123!'
-#         }
-#         response = api_client.patch(url, data, format='json')
-#         print(response)
-
-#         assert response.status_code == status.HTTP_200_OK
-#         assert response.data['status'] == 'OK'
-#         assert response.data['message'] == 'Password updated successfully'
 #         user.refresh_from_db()
 #         assert user.check_password('NewPassword123!')
 
-#     def test_update_password_without_old_password(self, api_client, mock_token, users):
-#         user = users[0]
-#         token = mock_token(user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+#     def test_update_password_without_old_password_failed(self):
+#         user = self.users[0]
+#         token = self.mock_token(user)
+#         self.api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
 
-#         url = reverse('user-update')
 #         data = {
-#             'id': user.id,
 #             'password': 'NewPassword123!',
 #         }
-#         response = api_client.patch(url, data, format='json')
+#         response = self.api_client.patch(self.url, data, format='json')
+#         print("response", response.data)
+
 
 #         assert response.status_code == status.HTTP_400_BAD_REQUEST
-#         assert response.data['status'] == 'FAILED'
-#         assert response.data['message'] == 'Validation failed'
-#         assert 'password' in response.data['errors']
+#         response_data = response.json()
+#         assert response_data['status'] == 'FAILED'
+#         assert response_data['message'] == 'Validation failed'
+#         assert 'old_password' in response_data['errors']
 
-#     def test_update_password_with_incorrect_old_password(self, api_client, mock_token, users):
-#         user = users[0]
-#         token = mock_token(user)
-#         api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+#     def test_update_password_with_incorrect_old_password_failed(self):
+#         user = self.users[0]
+#         token = self.mock_token(user)
+#         self.api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
 
-#         url = reverse('user-update')
 #         data = {
-#             'id': user.id,
 #             'password': 'NewPassword123!',
 #             'old_password': 'IncorrectOldPassword'
 #         }
-#         response = api_client.patch(url, data, format='json')
+#         response = self.api_client.patch(self.url, data, format='json')
 
 #         assert response.status_code == status.HTTP_400_BAD_REQUEST
-#         assert response.data['status'] == 'FAILED'
-#         assert response.data['message'] == 'Validation failed'
-#         assert 'old_password' in response.data['errors']
-
+#         response_data = response.json()
+#         assert response_data['status'] == 'FAILED'
+#         assert response_data['message'] == 'Validation failed'
+#         assert 'old_password' in response_data['errors']
