e108842fc7f367eb5083cbb71dc26dc1e3af5c8e
mie-jejaw <mierafjejaw@gmail.com>
2024-06-10 12:04:07 +0300

    implement get paginated packages list

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard/list-users', views.PaginatedUserListView.as_view(), name='paginated-list-users'),
+    path('dashboard/list-packages', views.PaginatedPackagePlanDetailsListView.as_view(), name='paginated-list-packages'),
     path('dashboard/subscriptions', views.SubscriptionPlanByPlanTypeListView.as_view(), name='subscription-plans-by-plan-type'),
     path('dashboard/convert/count', views.ConvertCountEachMonthView.as_view(), name='convert-count-each-month'),
     path('dashboard/activities/count', views.UserActivityCountView.as_view(), name='user-activity-count'),
